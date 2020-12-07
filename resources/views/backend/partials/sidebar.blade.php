db27c74055b4d37c77866ff8a8e4452c08500f43
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2020-12-07 13:18:49 +0600

    Update

---
+++ resources/views/backend/partials/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="sidebar-content">
+                <div class="nav-container">
+                    <nav id="main-menu-navigation" class="navigation-main">
+                        <div class="nav-lavel">Navigation</div>
+                        <div class="nav-item active">
+                            <a><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
+                        </div>
+
+
+                        <div class="nav-lavel">Menus</div>
+                        <div class="nav-item has-sub">
+                            <a href="#"><i class="ik ik-list"></i><span>Manage Slider</span></a>
+                            <div class="submenu-content">
+                                <a href="#" class="menu-item">View Sliders</a>
+                            </div>
+                        </div>
+                        <div class="nav-item has-sub">
+                            <a href="#"><i class="ik ik-list"></i><span>Manage Home</span></a>
+                            <div class="submenu-content">
+                                <a href="#" class="menu-item">Home</a>
+                            </div>
+                        </div>
+
+                        <div class="nav-item has-sub">
+                            <a href="#"><i class="ik ik-list"></i><span>Manage About</span></a>
+                            <div class="submenu-content">
+                                <a href="#" class="menu-item">About</a>
+                            </div>
+                        </div>
+
+                        <div class="nav-item has-sub">
+                            <a href="#"><i class="ik ik-list"></i><span>Manage Service</span></a>
+                            <div class="submenu-content">
+                                <a href="#" class="menu-item">Service</a>
+                            </div>
+                        </div>
+
+                    </nav>
+                </div>
+            </div>
